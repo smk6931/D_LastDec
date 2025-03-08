ec6910a9f79bf4ece5f26004b329dda5f01f6047
choiyeontaek <dusxorchl@naver.com>
2025-03-09 05:44:23 +0900

    소스 코드 오류
    경로 변경

--- Source/LastDec/CYT/Private/CYTPlayer.cpp
+++ Source/LastDec/CYT/Private/CYTPlayer.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "CYTPlayer.h"
+
+#include "LastDec/CYT/Public/CYTPlayer.h"
 
 // Sets default values
 ACYTPlayer::ACYTPlayer()
--- Source/LastDec/CYT/Private/LDGameModeBase.cpp
+++ Source/LastDec/CYT/Private/LDGameModeBase.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "LDGameModeBase.h"
 
+#include "LastDec/CYT/Public/LDGameModeBase.h"
