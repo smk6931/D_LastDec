e322840d9096502d0262816f772aa2f3ef8ecb86
choiyeontaek <dusxorchl@naver.com>
2025-03-09 15:29:23 +0900

    BP 클래스들 부모클래스로 C++ 클래스 상속받기 기본세팅 월드세팅 캐릭터랑 게임모드 지정

---
+++ Source/LastDec/CYTPlayer/CYTCharacter/DLAnim.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "DLAnim.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LASTDEC_API UDLAnim : public UAnimInstance
+{
+	GENERATED_BODY()
+};
--- Source/LastDec/CYTPlayer/CYTCharacter/DLPlayer.cpp
+++ Source/LastDec/CYTPlayer/CYTCharacter/DLPlayer.cpp
@@ -9,6 +9,8 @@ ADLPlayer::ADLPlayer()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	
 }
 
 // Called when the game starts or when spawned
